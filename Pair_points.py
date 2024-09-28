1b8e0275ba6364a477b57358953c7a7a98f43eeb
Gift Ayo <giftayo05@gmail.com>
2024-09-28 17:00:20 +0100

    floyd Algo

---
+++ Pair_points.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, val=None, nxt=None):
+        self.val = val
+        self.nxt = nxt
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+def cycle(list):
+    slow = list.head
+    fast = list.head
+    point = False
+    while fast is not None and fast.nxt is not None:
+        slow = slow.nxt
+        fast = fast.nxt.nxt
+        if slow == fast:
+            met = True
+            break
+    if not met:
+            return None
+    else:
+        slow = list.head
+        while  slow != fast:
+               slow = slow.nxt
+               fast = fast.nxt
+        return slow
+
+def find_double(bloc):
+    slow = 0
+    fast = 0
+    while True:
+        slow = bloc[slow]
+        fast = bloc[bloc[fast]]
+        if slow == fast:
+            break
+    slow = 0
+    while slow != fast :
+        slow = bloc[slow]
+        fast = bloc[fast]
+    return slow
+
+
+num = [4,3,2,7,8,6,9,2,1,5,2]
+result = LinkedList(num)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
